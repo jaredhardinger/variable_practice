1447fe9c5ccd17b123fac1ea5fe6c23f2347752f
Jared Hardinger <jaredhardinger@gmail.com>
2022-04-13 22:14:10 -0400

    Add annotation

--- variables.rb
+++ variables.rb
@@ -1,10 +1,11 @@
 a_number = 7
 another_number = 4259
-favorite_drink = water
-favorite_plant = tree
-a_float = 3.16
+favorite_drink = 'water'
+favorite_plant = 'tree'
+a_float = 3.16 #variable a_float is declared and assigned to float of 3.16
 another_float = 45.7
 red_is_color = true
 ball_is_square = false
-favorite_drink = seltzer
-favorite_plant = lilac
+favorite_drink = 'seltzer' #variable favorite_drink is reassigned to 'seltzer'
+favorite_plant = 'lilac' #this is a type of flower, allegedly
+
